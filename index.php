3fa700f7a6827c3f815a95591b1b99af1951aa14
fodana <df0037@mix.wvu.edu>
2021-02-03 18:02:19 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,55 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us!</h1>
+        <p>We are a highly functioning intelligent team that banded together to rate animal crossing characters. We are always right, no other rating is valid.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>Marshal</h2>
+                    <p>Role: Leader</p>
+                    <p>Species: Squirrel</p>
+                    <p>Personality: Smug</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpg">
+                <div class="container">
+                    <h2>Stiches</h2>
+                    <p>Role: Coordinator</p>
+                    <p>Species: Cub</p>
+                    <p>Personality: Lazy</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.png">
+                <div class="container">
+                    <h2>Merry</h2>
+                    <p>Role: Specialist</p>
+                    <p>Species: Cat</p>
+                    <p>Personality: Peppy</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>Molly</h2>
+                    <p>Role: Devil's Advocate</p>
+                    <p>Species: Duck</p>
+                    <p>Personality: Normal</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
