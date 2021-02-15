d427e1a15baca3bf09b5352cbbb210c531588f02
fodana <df0037@mix.wvu.edu>
2021-02-15 18:57:43 -0500

    Lab 4

--- signup.php
+++ signup.php
@@ -35,7 +35,7 @@ require "includes/header.php"
                         <input type="password" id="inputPassword" class="form-control" name="con-pwd" placeholder="Confirm Password"
                             required>
                         
-                        <button class="btn btn-lg btn-outline-primary btn-block" name="signup-submit" type="submit">Sign in</button>
+                        <button class="btn btn-lg btn-outline-primary btn-block" name="signup-submit" type="submit">Sign up</button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2021-9999</p>
                     </form>
                 </div>
